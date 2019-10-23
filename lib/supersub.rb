646d37712aceb68983b5c865c74813fbb1dae865
Nate Rudnick-Aldridge <nate.rudnickaldridge@gmail.com>
2019-10-22 23:21:33 -0500

    Initial commit

---
+++ lib/supersub.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+module SuperSub
+  #  x.split("").each_with_index do |a, i|
+  #    puts "\"#{a}\": \"#{y.split("")[i]}\","
+  #  end
+
+  SUBSCRIPT = {
+    "a": "ᵃ",
+    "b": "ᵇ",
+    "c": "ᶜ",
+    "d": "ᵈ",
+    "e": "ᵉ",
+    "f": "ᶠ",
+    "g": "ᵍ",
+    "h": "ʰ",
+    "i": "ⁱ",
+    "j": "ʲ",
+    "k": "ᵏ",
+    "l": "ˡ",
+    "m": "ᵐ",
+    "n": "ⁿ",
+    "o": "ᵒ",
+    "p": "ᵖ",
+    "q": "q",
+    "r": "ʳ",
+    "s": "ˢ",
+    "t": "ᵗ",
+    "u": "ᵘ",
+    "v": "ᵛ",
+    "w": "ʷ",
+    "x": "ˣ",
+    "y": "ʸ",
+    "z": "ᶻ",
+    "0": "⁰",
+    "1": "¹",
+    "2": "²",
+    "3": "³",
+    "4": "⁴",
+    "5": "⁵",
+    "6": "⁶",
+    "7": "⁷",
+    "8": "⁸",
+    "9": "⁹"
+  }
+  
+  def self.convert(str, style)
+    case style
+    when :subscript, :sub
+      str.split("").map{ |a| SUBSCRIPT[a.to_sym] }.join
+    end
+  end
+end
---
+++ supersub.gemspec
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+Gem::Specification.new do |s|
+  s.name = 'supersub'
+  s.version = '0.0.0'
+  s.date = '2019-10-22'
+  s.summary = 'Supersub converts strings into superscript, subscript, and other text forms'
+  s.files = ['lib/supersub.rb']
+  s.authors = ['Nate Rudnick-Aldridge']
+  s.email = 'nate.rudnickaldridge@gmail.com'
+  s.require_paths = ['lib']
+end
